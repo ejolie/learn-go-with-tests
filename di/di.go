bb7a62baa13624af0116a146063b074ae5904c73
ejolie <ejolie.dev@gmail.com>
2022-01-18 23:49:16 +0900

    8. Dependency Injection

---
+++ di/di.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
+
+	log.Fatal(http.ListenAndServe(":8080", http.HandlerFunc(MyGreeterHandler)))
+}
